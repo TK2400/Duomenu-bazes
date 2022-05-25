c6ed6c9a4fa55634fc76f69d6cbab76d79353412
T.Kav <tomas@plienas.lt>
2022-05-25 20:51:14 +0300

    useriu kolekcijoje randa tai ko prasome su "collection.find(...)";

--- index.js
+++ index.js
@@ -5,26 +5,42 @@ const mongoClient = new MongoClient("mongodb+srv://TK:TK2400@cluster0.y5udm.mong
 mongoClient.connect(function (err, client) {
     const database = client.db("usersdb");
     const collection = database.collection("users");
-    const newUsers = [
-      { name: "Tom", age: 30 },
-      { name: "Adam", age: 40 },
-      { name: "Jon", age: 60 },
-      { name: "Brad", age: 50 },
-      { name: "Tim", age: 15 },
-    ];
-    collection.insertMany(newUsers, function (err, result) {
-      if (err) {
-        return console.log("Failed to write an user");
-      }
-      collection.find().toArray(function (err, result) {
-        if (err) {
-          return console.log("errr", err);
-        }
-        console.log("Result:", result);
-        client.close();
-      });
-    });
-  });
+
+    // collection
+    //     .find({ name: "Brad" })
+    //     .toArray(function (err, result) {
+    //         console.log(result)
+    //         client.close()
+    //     })
+
+    collection
+    .find( {age: { $gt: 35} })
+    . toArray(function (err, result) {
+        console.log(result);
+        client.close()
+        return
+    })
+
+    // const newUsers = [
+    //     { name: "Tom", age: 30 },
+    //     { name: "Adam", age: 40 },
+    //     { name: "Jon", age: 60 },
+    //     { name: "Brad", age: 50 },
+    //     { name: "Tim", age: 15 },
+    // ];
+    // collection.insertMany(newUsers, function (err, result) {
+    //     if (err) {
+    //         return console.log("Failed to write an user");
+    //     }
+    //     collection.find().toArray(function (err, result) {
+    //         if (err) {
+    //             return console.log("errr", err);
+    //         }
+    //         console.log("Result:", result);
+    //         client.close();
+    //     });
+    // });
+});
 
 
 
