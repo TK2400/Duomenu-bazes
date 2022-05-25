1d13e75b4f46b2a82463e55aa535bcbb880f2f15
T.Kav <tomas@plienas.lt>
2022-05-25 20:27:15 +0300

    i duombaze prideda many users - masyva su objektais

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ const MongoClient = require("mongodb").MongoClient;
 
 const mongoClient = new MongoClient("mongodb+srv://TK:TK2400@cluster0.y5udm.mongodb.net/?retryWrites=true&w=majority")
 
-mongoClient.connect(function (error, client) {
-    const database = client.db("usersdb")
-    const collection = database.collection("users")
-    // const newUser = { name: "Steve", age: 30 }
-    // collection.insertOne(newUser, function (err, result) {
-    //     if (err) {
-    //         return console.log("failed to add an user")
-    //     }
-    //     console.log(result)
-    //     client.close()
-    // })
-collection.find().toArray(function(err, result) {
-    if(err) {
-        return console.log("err", err)
-    }
-
-    console.log("Result:", result)
-    client.close()
-})
-});
+mongoClient.connect(function (err, client) {
+    const database = client.db("usersdb");
+    const collection = database.collection("users");
+    const newUsers = [
+      { name: "Tom", age: 30 },
+      { name: "Adam", age: 40 },
+      { name: "Jon", age: 60 },
+      { name: "Brad", age: 50 },
+      { name: "Tim", age: 15 },
+    ];
+    collection.insertMany(newUsers, function (err, result) {
+      if (err) {
+        return console.log("Failed to write an user");
+      }
+      collection.find().toArray(function (err, result) {
+        if (err) {
+          return console.log("errr", err);
+        }
+        console.log("Result:", result);
+        client.close();
+      });
+    });
+  });
 
 
 
