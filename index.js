8c84069fb3763080496e2028ea8be0cf01a72776
T.Kav <tomas@plienas.lt>
2022-05-25 20:18:41 +0300

    sukuriamas naujas useris, dedamas i duomenu baze, po to atkomentuojant patikriname ka turime, susikuria 3 objektai su skirtinagais ID

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const MongoClient = require("mongodb").MongoClient;
 
 const mongoClient = new MongoClient("mongodb+srv://TK:TK2400@cluster0.y5udm.mongodb.net/?retryWrites=true&w=majority")
+
 mongoClient.connect(function (error, client) {
-  if (error) {
-    return console.log("Error was:", error);
-  }
-  console.log("Mongo connection success");
-  client.close();
+    const database = client.db("usersdb")
+    const collection = database.collection("users")
+    // const newUser = { name: "Steve", age: 30 }
+    // collection.insertOne(newUser, function (err, result) {
+    //     if (err) {
+    //         return console.log("failed to add an user")
+    //     }
+    //     console.log(result)
+    //     client.close()
+    // })
+collection.find().toArray(function(err, result) {
+    if(err) {
+        return console.log("err", err)
+    }
+
+    console.log("Result:", result)
+    client.close()
+})
 });
+
+
+
